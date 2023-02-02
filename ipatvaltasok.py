0f45873cfa7bd4a537a2d01dd1178f091e71f112
Nagy Kristóf <76815314+KristOOFF@users.noreply.github.com>
2023-02-02 09:25:37 +0100

    Add files via upload

--- ipatvaltasok.py
+++ ipatvaltasok.py
@@ -15,33 +15,28 @@ def atvaltasKettesSzamrendszerbe(szam):
 
     return eredmeny
 
-#def atvaltasTizesSzamrendszerbe(szam):
-    maradek = szam
-    eredmeny = ""
-
-    for i in [ 128, 64, 32, 16, 8, 4, 2 , 1 ]:
-        if maradek >= i:
-            maradek -= i
-            eredmeny += "128"
-        else:
-            eredmeny += "64"
-
-    return eredmeny
-    
+def atvaltasTizesSzamrendszerbe(szam):
+    szamjegyek = []
+    for i in range(0,len(szam)):
+        szamjegyek.append(int(szam[i]))
+    osszeg = 0
+    for i in range(0,len(szamjegyek)):
+        osszeg += szamjegyek[i]*2**(7-i)
+    return osszeg
 
 # 192.168.124.1 255.255.255.0 (11111111.11111111.11111111.00000000)
-
-ip2 = ""
-
-for i in ip.split("."):
-    szam2 = atvaltasKettesSzamrendszerbe(int(i))
-    ip2 += szam2
 
 #ip2 = ""
 
 #for i in ip.split("."):
-#    szam2 = atvaltasTizesSzamrendszerbe(int(i))
+#    szam2 = atvaltasKettesSzamrendszerbe(int(i))
 #    ip2 += szam2
+
+ip2 = ""
+
+for i in ip.split("."):
+    szam2 = atvaltasTizesSzamrendszerbe((i))
+    ip2 += szam2
 
 mask2 = ""
 
