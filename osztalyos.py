35f2e9bd322d824e7664c5d1d8894d1282fefd1a
GyulaBarnabas <92153211+GyulaBarnabas@users.noreply.github.com>
2023-02-06 11:08:09 +0100

    up date HU HA

--- osztalyos.py
+++ osztalyos.py
@@ -45,6 +45,14 @@ def osztalyhatarozo():
             if ip1 == 223:
                 if ip4 > 0:
                     print('Ez nem jóóóóóóó!')
+            if ip1 == 192 and ip2 == 168 and ip3 <= 1 and  ip3 >= 255:
+                print('Ez egy "C" osztályú cím és Belső cím!')
+        # "D" osztály
+        elif ip1 <= 224 and ip1 >= 239:
+            print('ez egy "D" osztályos cím, ezt nem oszthatóki mert ezek multicasting-es címek!!!')
+        #"E" osztály
+        elif ip1 <= 240 and ip1 >= 255:
+            print('Ez egy "D" osztályú cím nem osztható ki, mert internet címek!!!')
 
         
 
@@ -53,75 +61,3 @@ osztalyhatarozo()
         
 
 
-
-
-
-
-
-
-
-        
-"""
-        #A osztály
-        if  <= 127 and be[0] >= '1':
-            if be[0] == '10':
-                print('Ez egy "A" osztályú Belső cím',)
-            elif be == '127':
-                print('Ez a loopback cím, nem lehet kiosztani!',)
-            else:
-                print('Ez egy "A" osztályú cím!',)
-        #B osztály
-        elif be[0] >= '128' and be[0] == '191' :                
-            if be[0] == '172' and be[1] >= '16' and be[1] <= '31':
-                print('Ez egy "B" osztályú Belső cím')
-            else:
-                print('Ez egy "B" osztályú cím')
-        
-                
-                          
-
-oszthatarozo()
-
-
-"""
-
-
-#cucc
-"""
-from posixpath import split
-
-
-def osztaly():
-    #osztály meghatározó
-    while True:
-        be=input("Írja be az ip címet hogy metudja milyen osztályban van: ")
-        
-        be=be.split(".")
-
-        #Osztály meghatározás
-        
-        #A osztály
-        if be[0] <= '127' and be[0] >= '1':
-            if be[0] == '10':
-                print('Ez egy "A" osztályú Belső cím',)
-            elif be == '127':
-                print('Ez a loopback cím, nem lehet kiosztani!',)
-            else:
-                print('Ez egy "A" osztályú cím!',)
-
-        #B osztály
-        elif be[0] >= '128' and be[0] == '191' :                
-            if be[0] == '172' and be[1] >= '16' and be[1] <= '31':
-                print('Ez egy "B" osztályú Belső cím')
-            else:
-                print('Ez egy "B" osztályú cím')
-        
-        #C osztály
-        elif be[0] >= '192' and be[0] <= '223':
-            if be[0] == '192' and be[1] == '168' and be[2] == '1' and  be[2] >= '255':
-                print('Belső cím!') 
-                
-print('Osztály meghatározó!')
-osztaly()
-"""
-        
